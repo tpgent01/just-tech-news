340cb2fb4544ebcd87bbd3741eb11bfd4c795e52
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-10 21:06:07 -0600

    create put route for voting on a post - broken

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // GET all users
 router.get('/', (req, res) => {
@@ -62,6 +63,39 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
       });
 });
+
+
+// PUT /api.posts/upvote
+// must go before /:id PUT route, otherwise epress.js will think the word "upvote" is a valid paramter for /:id
+router.put('/upvote', (req,res) => {
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  }).then(() => {
+    // then find the post we just voted on
+    return Post.findOne({
+      where: {
+        id: req.body.post_id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+        [
+          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+          'vote_count'
+        ]
+      ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
 
 // PUT /api/post/1
 router.put('/:id', (req, res) => {
