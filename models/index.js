e1d2f8a4f68f3844ff8f5eaf0359c19d9c3eb23f
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-10 20:51:55 -0600

    created the vote model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
@@ -7,7 +8,38 @@ User.hasMany(Post, {
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
+    foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+  
+Post.belongsToMany(User, {
+    // connect Post and User through Vote, we want the foreignKey to be in Vote, and the model should be displayed as vote_posts when queried on.
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+// connects Votes to User by foriegnKey
+Vote.belongsTo(User, {
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+// connects user to vote by foriegnKey
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
