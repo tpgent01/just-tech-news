0c32f7406777f3de2aff4cf1a270dc52a4610493
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-10 21:46:42 -0600

    created api route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 // take routes and implement them to another router instance, prefixing them with the path /users
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
