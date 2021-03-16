9407b22386aa91553e4a471527f47f62e4c0f8d2
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-16 15:03:45 -0500

    conditionally rendered the login links

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,7 +31,10 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      res.render('homepage', { posts });
+      res.render('homepage', { 
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
